2b2b926a355f47df03111da9c14bc25092659d8d
sirakamiotoka <96097285+sirakamiotoka@users.noreply.github.com>
2025-05-14 21:11:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ clientDiscord.on(Events.MessageCreate, async message => {
   if (content === '!ik.kill') {
     if (voiceConnection) {
       voiceConnection.destroy();
-      voiceConnection = null;
+      //voiceConnection = null;
       activeChannel = null;
       message.reply('は？何してくれてんの？');
     } else {
