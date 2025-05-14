6e6b7be2004d1e90243052ec9a33031ce3b228ba
sirakamiotoka <96097285+sirakamiotoka@users.noreply.github.com>
2025-05-14 14:56:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -123,3 +123,15 @@ client.on('voiceStateUpdate', (oldState, newState) => {
 });
 
 client.login(process.env.BOT_TOKEN);
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running');
+});
+
+app.listen(3000, () => {
+  console.log('Web server is running on port 3000');
+});
+
