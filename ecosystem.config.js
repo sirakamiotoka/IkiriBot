3a1975e8ca5fa5a0efe42e50c0b386bfe06e7e19
sirakamiotoka <96097285+sirakamiotoka@users.noreply.github.com>
2025-08-20 23:57:10 +0900

    Update and rename dummyindex.js to ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "app",
+      script: "npm",
+      args: "start",
+      autorestart: true,
+      watch: false,
+      max_restarts: 10,
+      restart_delay: 5000, // 再起動間隔(ミリ秒)
+    },
+  ],
+};
