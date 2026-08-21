3be2dcfb8571c48cc78396af1e8255a407724a02
sirakamiotoka <96097285+sirakamiotoka@users.noreply.github.com>
2026-08-21 11:35:29 +0900

    Refactor ecosystem.config.js for ikiribot

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,33 +2,23 @@ module.exports = {
   apps: [
     {
       name: "ikiribot",
-      script: "index.js",   
+      script: "index.js",
+
       autorestart: true,
       watch: false,
-      
+
       restart_delay: 1500,
-      exp_backoff_restart_delay: 500
-    　min_uptime: "10s"
-     //2026-05-15 stop_exit_codes: [0, 2],
+
+      exp_backoff_restart_delay: 500,
+
       max_restarts: 20,
+      min_uptime: "10s",
+
+      max_memory_restart: "500M",
+
       env: {
         NODE_ENV: "production"
       }
     }
   ]
 };
-/*
-module.exports = {
-  apps: [
-    {
-      name: "app",
-      script: "npm",
-      args: "start",
-      autorestart: true,
-      watch: false,
-      max_restarts: 10,
-      restart_delay: 5000, // 再起動間隔(ミリ秒)
-    },
-  ],
-};
-*/
